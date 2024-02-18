a57d8db918277993e64a46be1922cfdf3fdd5d3a
mbledkowski <pub@mble.dk>
2024-02-18 16:55:26 +0100

    web: Update footer

--- web/components/footer.vue
+++ web/components/footer.vue
@@ -6,15 +6,20 @@
       </picture>
     </UDivider>
     <div class="contents">
-      <p>Source code - <ULink to="https://github.com/mbledkowski/inoutnft"
+      <p>{{ $t('source') }} - <ULink to="https://github.com/mbledkowski/inoutnft"
           class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200" target="_blank">
           GitHub
         </ULink>
       </p>
-      <p>Created by <ULink to="https://github.com/mbledkowski"
+      <p>{{ $t('createdby') }} <ULink to="https://mble.dk"
           class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200" target="_blank">
-          @mbledkowski
+          {{ $t('createdbyname') }}
         </ULink>
+      </p>
+    </div>
+    <div class="legal">
+      <p>
+        {{ $t('legalMumboJumbo') }}
       </p>
     </div>
   </footer>
@@ -25,6 +30,8 @@ footer {
   display: flex;
   flex-flow: column nowrap;
   align-items: center;
+  @apply h-fit;
+  @apply bg-gray-50 dark:bg-gray-950;
 
   &>.divider picture {
     width: 32px;
@@ -32,7 +39,8 @@ footer {
     @apply dark:invert;
   }
 
-  &>.contents {
+  &>.contents,
+  &>.legal {
     @apply text-sm;
     display: flex;
     justify-content: space-between;
@@ -40,6 +48,11 @@ footer {
     @apply w-full max-w-screen-sm;
     @apply items-center;
     @apply flex-col sm:flex-row;
+    @apply pb-2;
+  }
+
+  &>.legal {
+    @apply text-justify;
   }
 }
 </style>
--- web/config/i18n.config.ts
+++ web/config/i18n.config.ts
@@ -4,11 +4,29 @@ export default defineI18nConfig(() => ({
   messages: {
     en: {
       welcome: 'Welcome',
-      login: 'Connect wallet'
+      login: 'Connect wallet',
+      motto: 'Trade your Game Items on DEX',
+      steamnft: 'Steam Items to NFT',
+      nftsteam: 'NFT to Steam Items',
+      howitworks: 'How it works?',
+      start: "Start now",
+      source: "Source code",
+      createdby: "Created by",
+      createdbyname: "Maciej Błe˛dkowski",
+      legalMumboJumbo: "This service is not affiliated in any way with Valve Corp. and its affiliates (collectively the “Valve”). We do not claim any rights to the virtual items, which remain the intellectual property of Valve Corp."
     },
     pl: {
       welcome: 'Witaj',
-      login: 'Połącz portfel'
+      login: 'Połącz portfel',
+      motto: 'Handluj swoimi Przedmiotami Z Gier na Zdecentralizowanym Rynku',
+      steamnft: 'Przedmioty Steam na NFT',
+      nftsteam: 'NFT na Przedmioty Steam',
+      howitworks: 'Jak to działa?',
+      start: "Zaczynamy",
+      source: "Kod źródłowy",
+      createdby: "Stworzone przez",
+      createdbyname: "Macieja Błe˛dkowskiego",
+      legalMumboJumbo: "Ta usługa nie jest w żaden sposób powiązana z Valve Corp. i powiązanymi podmiotami (kolektywnie „Valve”). Nie rościmy sobie jakiechkolwiek praw do wirtualnych przedmiotów, które pozostają własnością intelektualną Valve Corp."
     }
   }
 }))
